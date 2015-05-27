51ffb91ecc9a42704f52c490c32541d8ded21468
bharat <student@Achiever23.local>
2015-05-27 17:05:56 +0530

    ok

--- SQLiteDemo/ViewController.h
+++ SQLiteDemo/ViewController.h
@@ -2,12 +2,28 @@
 //  ViewController.h
 //  SQLiteDemo
 //
-//  Created by Manjunadh Bommisetty on 29/03/13.
+//  Created by Manjunadh Bommisetty on 07/11/14.
 //  Copyright (c) 2013 Manjunadh Bommisetty. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "sqlite3.h"
 
 @interface ViewController : UIViewController
+
+@property UILabel * nameLbl;
+@property UILabel * addressLbl;
+@property UILabel * phoneLbl;
+@property UILabel * statusLbl;
+@property UITextField * nameTF;
+@property UITextField * addressTF;
+@property UITextField * phoneTF;
+
+@property UIButton * saveBtn;
+
+@property UIButton * findBtn;
+
+@property sqlite3 * contactDB;
+
 
 @end
